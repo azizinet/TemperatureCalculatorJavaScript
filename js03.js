4619f23495de7690efa41032b22fda4dddee9950
Azizi NET <azizinet00@outlook.com>
2021-07-14 21:02:33 +0800

    Added result innerHTML text

--- js03.js
+++ js03.js
@@ -1,32 +1,36 @@
 var fahrenheit = document.getElementById('fahrenheit');
 var celsius = document.getElementById('celsius');
 var form = document.getElementById('fahrenheitCelsiusConversion');
+var resultFC = document.getElementById('resultFahrenheitCelsius');
 
 form.addEventListener('submit', function(event) {
     if (fahrenheit.value && celsius.value) {
-        alert("Please insert only one output");
+        alert("Sila letakkan hanya satu nilai Fahrenheit atau Celsius");
         event.preventDefault();
     } 
-    
+
     else if (celsius.value) {
         var result = 9 * celsius.value / 5 + 32;
         fahrenheit.value = result;
+        resultFC.innerHTML = celsius.value + " &#8451; = " + fahrenheit.value + " &#8457;.";
         event.preventDefault();
     }
 
     else if (fahrenheit.value) {
         var result = 5 * (fahrenheit.value - 32) / 9;
         celsius.value = result;
+        resultFC.innerHTML = fahrenheit.value + " &#8457; = " + celsius.value + " &#8451;.";
         event.preventDefault();
     }
 
     else {
-        alert("Please insert at least one output");
-        event.preventDefault();
+    alert("Sila letakkan nilai Fahrenheit atau Celsius");
+    event.preventDefault();
     }
 });
 
 function refresh() {
     fahrenheit.value = "";
     celsius.value = "";
+    resultFC.innerHTML = "<br>";
 }
